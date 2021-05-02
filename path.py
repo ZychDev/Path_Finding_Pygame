63a5694039895022719dc7236374923f0135ca4b
ZychDev <szymonzychdev@gmail.com>
2021-05-02 14:46:02 +0200

    draw blue and red positions

--- path.py
+++ path.py
@@ -4,6 +4,8 @@ pygame.init()
 #colors
 black = [0, 0, 0]
 red = [255, 0, 0]
+green = [0,255,0]
+blue = [0,0,255]
 
 #screen settings
 screen_size = width, height = 800, 800
@@ -14,49 +16,91 @@ screen.fill(black)
 size = 20
 size_rect =int( width / size)
 test = 5
-array = [[0 for i in range(test)] for j in range(test)]
 
 #class rectangle
 class point:
-	def __init__(self, x, y, color):
-		self.x = x 
-		self.y = y 
+	x = 0
+	y = 0
+
+	def __init__(self,color):
 		self.color = color
+
+	def set_position(self, x, y):
+		self.x = x
+		self.y = y
 
 	def position(self):
 		return self.x, self.y
+	
+	def position_x(self):
+		return self.x
+	
+	def position_y(self):
+		return self.y
 
-#functions
-def create_array(array,width,size_rect,red):
-	x = 0
-	y = 0
-	for i in array:
-		for j in i:
-			if y == width:
-				y = 0
-				print(hello)
-			else:
-				j = point(x,y,red)
-				print(j.x , j.x)
-				y += 20
+	def change_color(self, col):
+		self.color = col
+
+	def return_color():
+		return self.color
+
+#create array of objects and input informations 
+array_of_objects = [[point(red) for j in range(test)] for i in range(test)]
+
+def start_point():
+	x = int(input("Give point x: "))
+	y = int(input("Give point y: "))
+	return x,y
+
+def quit_point():
+	x = int(input("Give point x: "))
+	y = int(input("Give point y: "))
+	return x,y
+
+start_points = start_point()
+quit_points = quit_point()
+
+print("Start points: ",start_points)
+print("Quit points: ",quit_points)
+
+#save data in rectangle objects
+tmpx = 0
+tmpy = 0
+for i in array_of_objects:
+	for z in i:
+		z.set_position(tmpx,tmpy)	
+
+		if z.position() == start_points:
+			z.change_color(green)
+		elif z.position() == quit_points:
+			z.change_color(blue)
+
+		tmpx += 20
+	if(tmpx == 100):
+		tmpy += 20
+		tmpx = 0
+
 
 
 
 #main loop
-
-create_array(array, width, test, red)
-
 start = True
 while start:
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			start = False
 		elif event.type == pygame.KEYDOWN:
-			if event.key == K_ESCAPE:
+			if event.key == pygame.K_ESCAPE:
 				start = False
 
-
+	#draw rectaangles
+	for z in array_of_objects:
+		for c in z:
+			pygame.draw.rect(screen, c.color, (c.x,c.y,20,20),1)
+		
 	pygame.display.flip()
+
 
 
 
